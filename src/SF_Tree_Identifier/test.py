496cbfe31fb8cfcc5529b57a63f2fbe4edd59d33
Zaven <zcook7904@gmail.com>
2022-12-07 07:08:47 -0800

    Moved tests to module

--- src/SF_Tree_Identifier/test.py
+++ src/SF_Tree_Identifier/test.py
@@ -1,8 +1,10 @@
 import os
 import pytest
 
-root_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+root_dir = os.path.dirname(__file__)
 TEST_DIRECTORY = os.path.join(root_dir, 'tests')
-print(TEST_DIRECTORY)
 def test():
     pytest.main([TEST_DIRECTORY])
+
+if __name__ == "__main__":
+    test()
