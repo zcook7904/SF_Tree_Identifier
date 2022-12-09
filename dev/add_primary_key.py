58cb9248cda5936619062c2535c5d7092091fd4a
Zaven <zcook7904@gmail.com>
2022-12-08 22:01:28 -0800

    Added primary key to species table in the SF_Trees DB

---
+++ dev/add_primary_key.py
@@ -0,0 +1,28 @@
+import sqlite3
+import os
+
+os.chdir('..')
+path = f"src//SF_Tree_Identifier//data//SF_trees.db"
+
+con = sqlite3.connect(path)
+cur = con.cursor()
+query = """
+PRAGMA foreign_keys=off
+ALTER TABLE 'species' RENAME to old_species
+
+CREATE TABLE "species" (
+"index" INTEGER PRIMARY KEY,
+  "qSpecies" TEXT,
+  "urlPath" INTEGER
+)
+
+"""
+result = cur.execute(query)
+
+if result:
+    if fetchall:
+        result = result.fetchall()
+    else:
+        result = result.fetchone()
+
+con.close()
